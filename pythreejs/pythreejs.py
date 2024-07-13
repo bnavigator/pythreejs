af194ff85f79741f21a95b7869325aba253f7040
Ben Greiner <code@bnavigator.de>
2024-07-13 15:17:58 +0200

    replace deprecated matplotlib.cm.get_cmap()

--- pythreejs/pythreejs.py
+++ pythreejs/pythreejs.py
@@ -231,10 +231,10 @@ def make_text(text, position=(0, 0, 0), height=1):
 
 def height_texture(z, colormap='viridis'):
     """Create a texture corresponding to the heights in z and the given colormap."""
-    from matplotlib import cm
+    from matplotlib import colormaps
     from skimage import img_as_ubyte
 
-    colormap = cm.get_cmap(colormap)
+    colormap = colormaps[colormap]
     im = z.copy()
     # rescale to be in [0,1], scale nan to be the smallest value
     im -= np.nanmin(im)
